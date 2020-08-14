6e872cbf536179301973574228c23fa90cb56405
lyger <lygerzero0zero@gmail.com>
2020-08-14 18:13:59 +0900

    Updates

---
+++ precache-manifest.03d6c5d3e1d7b48950b1f45628620437.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01a264e17e505963bb7514a1a3f57c0b",
+    "url": "/matsurisu_en/index.html"
+  },
+  {
+    "revision": "5a0b825df52049fa312d",
+    "url": "/matsurisu_en/static/css/main.3bb1920f.chunk.css"
+  },
+  {
+    "revision": "c8aff473c8e451cf5f63",
+    "url": "/matsurisu_en/static/js/2.1f7fa79e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/matsurisu_en/static/js/2.1f7fa79e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a0b825df52049fa312d",
+    "url": "/matsurisu_en/static/js/main.878ec49e.chunk.js"
+  },
+  {
+    "revision": "47f16178435b3f038872",
+    "url": "/matsurisu_en/static/js/runtime-main.18f0719e.js"
+  }
+]);
